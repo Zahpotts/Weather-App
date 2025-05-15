ae339483ad4023b9b25bafeb853b876b03c7f4e4
Isaiah <zahpotts123@gmail.com>
2025-05-15 12:45:12 -0400

    fixed everything

--- vite.config.js
+++ vite.config.js
@@ -16,10 +16,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target:'http://localhost:5000',
-        changeOrigin: true,
-      },
+      '/api': 'http://localhost:5000',
     },
   },
 })
