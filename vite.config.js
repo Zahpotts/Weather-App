35741fdbbcf3faa4448ba5a75560e62331aedf26
Isaiah <zahpotts123@gmail.com>
2025-05-15 12:32:52 -0400

    updated file

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,10 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': {
+        target:'http://localhost:5000',
+        changeOrigin: true,
+      },
     },
   },
 })
